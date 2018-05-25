393880d56c59aadcf11543de7604471c5f00fdb0
Arun Balakrishnan <arun.balakrishnan@live.com>
2018-05-25 11:54:15 +0530

    toast remove

--- XamlPlayground/Playground.xaml.cs
+++ XamlPlayground/Playground.xaml.cs
@@ -7,39 +7,14 @@ namespace XamarinFormsStarterKit.UserInterfaceBuilder.XamlPlayground
 	{
 		void Handle_Clicked(object sender, System.EventArgs e)
 		{
-			ShowToast(new NotificationOptions()
-            {
-                Title = "The Title Line",
-                Description = "The Description Content",
-                IsClickable = true,
-                WindowsOptions = new WindowsOptions() { LogoUri = "icon.png" },
-                ClearFromHistory = false,
-                AllowTapInNotificationCenter = false,
-                AndroidOptions = new AndroidOptions()
-                {
-                    HexColor = "#F99D1C",
-                    ForceOpenAppOnNotificationTap = true
-                }
-            });
+             
 
  		}
-		void ShowToast(INotificationOptions options)
-        {
-            var notificator = DependencyService.Get<IToastNotificator>();
-
-            // await notificator.Notify(options);
-
-            notificator.Notify((INotificationResult result) =>
-            {
-                System.Diagnostics.Debug.WriteLine("Notification [" + result.Id + "] Result Action: " + result.Action);
-            }, options);
-        }
+         
 		public Playground()
 		{
 			InitializeComponent();
-
-             
-
+            
 			ComponentBuilder.Init(new ComponentBuilderOptions
 			{
 				Content = Content,
