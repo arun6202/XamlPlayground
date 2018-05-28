9846b02f6f0e86a3d86745c49bd784d42b0bef54
Arun Balakrishnan <arun.balakrishnan@live.com>
2018-05-28 13:58:58 +0530

    create seaparte xaml

---
+++ XamlPlayground/References/BogusGeneratorWithoutParam.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace XamarinFormsStarterKit.UserInterfaceBuilder.XamlPlayground.References
+{
+    public partial class BogusGeneratorWithoutParam : ContentView
+    {
+        public BogusGeneratorWithoutParam()
+        {
+            InitializeComponent();
+        }
+    }
+}
