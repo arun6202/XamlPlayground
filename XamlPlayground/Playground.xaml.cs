245fcd2cd8237401373c0fa44896a9f98b6b045e
Arun Balakrishnan <arun.balakrishnan@live.com>
2018-05-29 20:09:55 +0530

    layout fix

--- XamlPlayground/Playground.xaml.cs
+++ XamlPlayground/Playground.xaml.cs
@@ -5,37 +5,36 @@ using Xamarin.Forms;
 
 namespace XamarinFormsStarterKit.UserInterfaceBuilder.XamlPlayground
 {
-    public partial class Playground : ContentPage
-    {
-        public Playground()
-        {
-            InitializeComponent();
+	public partial class Playground : ContentPage
+	{
+		public Playground()
+		{
+			InitializeComponent();
 
-
-            ComponentBuilder.Init(new ComponentBuilderOptions
-            {
-                Content = Content,
-                PreserveSession = true,
-                SuppressAllBackGroundColor = false,
+			ComponentBuilder.Init(new ComponentBuilderOptions
+			{
+				Content = Content,
+				PreserveSession = true,
+				SuppressAllBackGroundColor = false,
 				EnableRepeater = true,
 				EnableRestorationOfUIAttributes = true,
-                EnableTapGestureRecognizers = true,
-                EnableUIAttributesGeneration = false,
-				CompressLayout=true,
-				Apply=true
-                
-            });
+				EnableTapGestureRecognizers = true,
+				EnableUIAttributesGeneration = false,
+				CompressLayout = false,
+				Apply = true
 
-        }
+			});
+
+		}
 
 		public Playground(ComponentBuilderOptions options)
-        {
-            InitializeComponent();
+		{
+			InitializeComponent();
 
 			options.Content = Content;
-            
+
 			ComponentBuilder.Init(options);
 
-        }
-    }
+		}
+	}
 }
