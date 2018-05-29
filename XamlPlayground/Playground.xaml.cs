4839c87628746800aaa37df644388ab3780e5a82
Arun Balakrishnan <arun.balakrishnan@live.com>
2018-05-29 10:45:58 +0530

    config options

--- XamlPlayground/Playground.xaml.cs
+++ XamlPlayground/Playground.xaml.cs
@@ -12,13 +12,28 @@ namespace XamarinFormsStarterKit.UserInterfaceBuilder.XamlPlayground
             InitializeComponent();
 
 
-//ComponentBuilder.Init(new ComponentBuilderOptions
-            //{
-            //    Content = Content,
-            //    PreserveSession = false,
-            //    SuppressAllBackGroundColor = false
+            ComponentBuilder.Init(new ComponentBuilderOptions
+            {
+                Content = Content,
+                PreserveSession = false,
+                SuppressAllBackGroundColor = false,
+				EnableRepeater = false,
+                EnableRestorationOfUIAttributes = false,
+                EnableTapGestureRecognizers = true,
+                EnableUIAttributesGeneration = false,
+				CompressLayout=false
 
-            //});
+            });
+
+        }
+
+		public Playground(ComponentBuilderOptions options)
+        {
+            InitializeComponent();
+
+			options.Content = Content;
+            
+			ComponentBuilder.Init(options);
 
         }
     }
---
+++ XamlPlayground/References/ComponentBuilderOptionsUsage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace XamarinFormsStarterKit.UserInterfaceBuilder.XamlPlayground.References
+{
+    public class ComponentBuilderOptionsUsage
+    {
+        public ComponentBuilderOptionsUsage()
+        {
+			ComponentBuilder.Init(new ComponentBuilderOptions
+            {
+                // Content = Content,
+                PreserveSession = false,
+                SuppressAllBackGroundColor = false,
+                EnableRepeater = false,
+                EnableRestorationOfUIAttributes = false,
+                EnableTapGestureRecognizers = false,
+                EnableUIAttributesGeneration = false
+
+            });
+        }
+    }
+}
