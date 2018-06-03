1b19cac01facd2e9cad81b9e3c4a39f930969e09
Arun Balakrishnan <arun.balakrishnan@live.com>
2018-06-03 23:07:07 +0530

    live xaml

---
+++ XamlPlayground/Helpers/LiveXamlHelper.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinFormsStarterKit.UserInterfaceBuilder.XamlPlayground.Helpers
+{
+    public class LiveXamlHelper
+    {
+        public LiveXamlHelper()
+        {
+        }
+
+		void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            var xaml = "<StackLayout><Label Text=\"I am  modified Xamarin forms!\" HorizontalOptions=\"Center\" VerticalOptions=\"CenterAndExpand\" /></StackLayout>";
+
+            PreviewXaml(xaml,null).Wait();
+
+        }
+
+		async Task PreviewXaml(string xaml,ContentPage contentPage)
+        {
+ 
+            try
+            {
+                if (string.IsNullOrEmpty(xaml))
+                    return;
+
+                string contentPageXaml = $"<?xml version='1.0' encoding='utf-8' ?><ContentPage xmlns='http://xamarin.com/schemas/2014/forms' xmlns:x='http://schemas.microsoft.com/winfx/2009/xaml' x:Class ='XamarinFormsStarterKit.XamlPage'>{xaml}</ContentPage>";
+
+				await LiveXaml.UpdatePageFromXamlAsync(contentPage, contentPageXaml);
+            }
+            catch (Exception exception)
+            {
+                // Error 
+                Debug.WriteLine(exception.Message);
+				var xamlException = LiveXaml.GetXamlException(exception);
+				await LiveXaml.UpdatePageFromXamlAsync(contentPage, xamlException);
+            }
+
+        }
+
+
+
+    }
+}
