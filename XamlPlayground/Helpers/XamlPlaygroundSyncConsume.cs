7345a9255b6c6c45f75b799ebe75ba5e341816ce
Arun Balakrishnan <arun.balakrishnan@live.com>
2018-06-09 15:04:55 +0530

    push

--- XamlPlayground/Helpers/XamlPlaygroundSyncConsume.cs
+++ XamlPlayground/Helpers/XamlPlaygroundSyncConsume.cs
@@ -1,10 +1,52 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR.Client;
+using XamarinFormsStarterKit.UserInterfaceBuilder.Models;
+
 namespace XamarinFormsStarterKit.UserInterfaceBuilder.XamlPlayground.Helpers
 {
-    public class XamlPlaygroundConsume
-    {
-        public XamlPlaygroundConsume()
-        {
-        }
-    }
+	public static class XamlPlaygroundSyncConsume
+	{
+		private const string Url = "http://localhost:6202/XamlPlaygroundSync";
+		private static HubConnection _connection;
+
+		static XamlPayload XamlPayload = new XamlPayload();
+
+		internal static async Task Sync()
+		{
+			await StartConnectionAsync();
+
+			_connection.On<string, XamlPayload>("XamlPlaygroundSync", async (name, message) =>
+			{
+
+				if (XamlPayload.XAML == null)
+				{
+					await LiveXamlHelper.PreviewXaml(message.XAML,App.ContentPageSync);
+				}
+
+				bool areXamlSame = XamlPayload.CompareTo(message) == 0;
+				if (!areXamlSame)
+				{
+					XamlPayload = message;
+					await LiveXamlHelper.PreviewXaml(message.XAML, App.ContentPageSync);
+				}
+			});
+
+		}
+
+
+		public static async Task StartConnectionAsync()
+		{
+			_connection = new HubConnectionBuilder()
+				.WithUrl(Url)
+				.Build();
+
+			await _connection.StartAsync();
+		}
+
+		public static async Task DisposeAsync()
+		{
+			await _connection.DisposeAsync();
+		}
+	}
 }
